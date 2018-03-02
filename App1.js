2814fb2310be066af3f435bd341e32aa8f204089
tanakorn0314 <tanakorn0412@gmail.com>
2018-03-02 14:58:22 +0700

    first

---
+++ App1.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+import './App.css';
+
+class App extends Component{
+    render(){
+        return ( <div>
+                <h1 className="App-header">
+                Hello World
+                </h1>
+            </div>
+        );
+
+    }
+}
+
+export default App;
---
+++ App2.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class App extends Component{constr
+}
+
+class Foo extends Component{
+    render(){
+        return (
+                 <div>
+                    <h2>{this.props.name} {this.props.surname}</h2>
+                     For for For for For for  <br/>
+                     For for For for For for  <br/>
+                     For for For for For for  <br/>
+                 </div>
+              );
+    }
+}
+
+export default App;
